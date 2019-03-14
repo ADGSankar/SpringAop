502c25565802d9f9d241e5ffc9d2a900f60c6377
A.D.G.Sankar <a.d.g.sankar@gmail.com>
2019-03-14 18:19:38 +0530

    Spring AOP - combining more then one pointcut into single point cut,bean interception and within interception

--- src/main/java/com/example/spring/aop/springaop/aspect/CommonJoinPointConfig.java
+++ src/main/java/com/example/spring/aop/springaop/aspect/CommonJoinPointConfig.java
@@ -10,5 +10,15 @@ public class CommonJoinPointConfig {
 	@Pointcut("execution (* com.example.spring.aop.springaop.business.*.*(..))")
 	public void businessLayerExecution() {}
 	
-
+	@Pointcut("com.example.spring.aop.springaop.aspect.CommonJoinPointConfig.dataLayerExecution() && com.example.spring.aop.springaop.aspect.CommonJoinPointConfig.businessLayerExecution()")
+	public void anyLayerExecution() {}
+	
+	@Pointcut("bean(*Dao*)")
+	public void anyBeanContainingDao() {}
+	
+	@Pointcut("within(com.example.spring.aop.springaop.data..*)")
+	public void dataLayerExecutionWithWithin() {}
+	
+	
+	
 }
